ab101faa5c8987b7236433588fa8bf9b8ede0bc1
Ayusman-Singhal <ayusmansinghal@gmail.com>
2025-06-11 23:53:18 +0530

    delete and update redundant index.ts file

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import dotenv from "dotenv";
+import { appRouter } from "./src/trpc/router";
+
+dotenv.config();
+
+const port = Number(process.env.PORT || 2025);
+
+createHTTPServer({ router: appRouter }).listen(port);
+
+console.log(`🚀 Server running at http://localhost:${port}`);
