0cf3c63fa357764aa36ef3643565e0873a51c379
Madhav Jangid <manumadhavjangid@gmail.com>
2025-06-14 01:56:25 +0530

    Complete Hero section and Footer section
    Completed both sections with exact measurement's according to figma and proper responsiveness
    in mobile view of the screen the Eclypse text don't match the color according to figma but in figma it has a bluish glow on top it which comes under the background part so every thing is fine from my side..

---
+++ frontend/src/components/Been-Pulse/index.tsx
@@ -0,0 +1,58 @@
+import beemSvg from '../../../public/assets/svg/beem_pulse.svg';
+import EclypseLogo from '../../../public/assets/icons/EclypseLogo.png';
+import { motion, useScroll, useTransform } from "framer-motion";
+
+export default function Beem_pulse() {
+
+
+
+  const { scrollY } = useScroll();
+
+  const rotation = useTransform(scrollY, [0, 210], [120, 0]);
+
+
+
+  return (
+    <div className='relative min-h-auto lg:mb-0 md:mb-16 mb-5 lg:min-h-screen w-full'>
+      <div className='w-full h-full'>
+        <div className='relative lg:absolute flex flex-col justify-start items-center w-full overflow-clip'>
+          <div className="flex items-center justify-center w-full overflow-hidden relative mt-[-5px]">
+            <img
+              src={beemSvg}
+              alt="beemSvg"
+              className="centerBeam min-w-[1400px] sm:min-w-[1500px] max-w-full "
+            />
+            <img
+              src={beemSvg}
+              alt="beemSvg"
+              className="min-w-[1400px] sm:min-w-[1500px] max-w-full absolute  top-1/2 left-1/2 transform -translate-y-1/2 z-[11] -translate-x-1/2"
+            />
+          </div>
+
+          <div className='block h-28 w-2 rounded-full blur-md bg-[#fff] mx-auto absolute top-40 left-1/2 transform -translate-x-1/2 -translate-y-1/2 topDownAnimation'></div>
+          <div className='block h-16 w-2 rounded-full blur-xl bg-[#fff] mx-auto absolute top-40 left-1/2 transform -translate-x-1/2 -translate-y-1/2 topDownAnimation2'></div>
+          <div className='h-14 w-20  aspect-square blur-lg  rounded-full bg-[#ffffff] absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-[-20px]  '></div>
+
+
+          <motion.img
+            src={EclypseLogo}
+            alt="beemSvg"
+            className="centerLogo min-w-[400px] max-w-1/2 absolute mt-[-7px] z-[9] top-1/2 ml-[5px]  "
+            style={{
+              y: "-24%",
+              rotate: rotation,
+            }}
+          />
+          <motion.img
+            src={EclypseLogo}
+            alt="beemSvg"
+            className="max-w-1/2 min-w-[400px] absolute mt-[-7px] z-[10] top-1/2 ml-[5px] "
+            style={{
+              y: "-24%",
+              rotate: rotation,
+            }} />
+        </div>
+      </div>
+    </div >
+  )
+}
---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,53 @@
+
+import InstagramIcon from "./icons/InstagramIcon";
+import LinkedInIcon from "./icons/LinkedInIcon";
+import TwitterIcon from "./icons/TwitterIcon";
+import OrbitFooter from "./OrbitFooter";
+import type { OrbitIcon, Breakpoints } from "./types";
+
+
+
+
+const iconList: OrbitIcon[] = [
+  {
+    icon: <InstagramIcon />,
+    url: "https://instagram.com",
+    delay: "0",
+  },
+  {
+    icon: <TwitterIcon />,
+    url: "https://twitter.com",
+    delay: "4",
+  },
+  {
+    icon: <LinkedInIcon />,
+    url: "https://linkedin.com",
+    delay: "8",
+  },
+];
+
+
+
+const breakpoints: Breakpoints = {
+  defaultRadiusXRatio: 0.502,
+  defaultRadiusYRatio: 0.7,
+  mobileHeight: 150,
+  desktopHeight: 450,
+  responsiveThreshold: 1025,
+};
+
+const Footer = () => {
+
+  return (
+    <div className="w-full min-h-screen relative flex items-end justify-center overflow-hidden z-[110]">
+      <OrbitFooter icons={iconList} breakpoints={breakpoints} />
+      <p className="absolute bottom-12 text-white  font-normal text-[14px] md:text-[16.25px] leading-[150%] tracking-[0] text-center w-full opacity-70">
+        Eclypse Innovations © Pvt. Ltd.
+      </p>
+
+      <div className="block  w-full h-72 transform translate-y-1/2 blur-3xl rounded-[100%] rounded-b-0 bg-[#3d3d9a20] " />
+    </div>
+  );
+};
+
+export default Footer;
