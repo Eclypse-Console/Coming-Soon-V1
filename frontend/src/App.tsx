1ec5effacd9a48cb39314f7ac903a9ddab0b8ce1
Ayusman-Singhal <ayusmansinghal@gmail.com>
2025-06-14 15:45:07 +0530

    background for the coming soon page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,14 @@ import Footer from "./components/footer/index";
 import NewsLetter from "./components/NewsLetter";
 import FlipClock from "./components/FlipClock";
 import HeroSection from "./views/Hero";
+import DualTopographicCanvas from "./components/TopographiCanvas";
 
 function mainPage() {
 	return <div className="bg-black w-full h-full relative">
+
+		<div className="fixed inset-0 w-full h-full z-0">
+			<DualTopographicCanvas />
+		</div>
 		<HeroSection />
 		<FlipClock />
 		<NewsLetter />
