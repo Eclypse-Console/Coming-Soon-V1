841d085c497114bc28ffa1386317196b59ddd833
GarPav123 <garimellapavan.teja2022@vitstudent.ac.in>
2025-06-14 18:54:44 +0530

    fix : removed scrollbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,19 @@ import FlipClock from "./components/FlipClock";
 import HeroSection from "./views/Hero";
 import GlowVectors from "./components/GlowVectors";
 import DualTopographicCanvas from "./components/TopographiCanvas";
-import CustomScrollbar from "./components/CustomScrollbar";
 
 function mainPage() {
 	return <div className="bg-black w-full h-full relative">
-		<CustomScrollbar className="h-full w-full no-scrollbar">
+		{/* <CustomScrollbar className="h-full w-full no-scrollbar"> */}
 		<div className="fixed inset-0 w-full h-full z-0">
 			<DualTopographicCanvas />
 		</div>
-			<HeroSection />
-			<GlowVectors />
-			<FlipClock />
-			<NewsLetter />
-			<Footer />
-		</CustomScrollbar>
+		<HeroSection />
+		<GlowVectors />
+		<FlipClock />
+		<NewsLetter />
+		<Footer />
+		{/* </CustomScrollbar> */}
 
 	</div>
 }
